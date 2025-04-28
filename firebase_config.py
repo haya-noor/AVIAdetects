38350c619b97b7e579ea11decf2297490c1752f8
Haya Noor <hayanoor.skt@gmail.com>
2025-04-28 21:28:21 +0500

    Add files via upload

---
+++ firebase_config.py
@@ -0,0 +1,12 @@
+# firebase_config.py
+
+firebase_config = {
+    "apiKey": "AIzaSyDbHHcSft8xP8QqesNZFVPdLZDPHldt6_w",
+    "authDomain": "aviadetects.firebaseapp.com",
+    "projectId": "aviadetects",
+    "storageBucket": "aviadetects.appspot.com",
+    "messagingSenderId": "704167978390",
+    "appId": "1:704167978390:web:de8ff19cafb4d4377ba9b7",
+    "measurementId": "G-XFL4FZCKLB",
+    "databaseURL": "https://console.firebase.google.com/u/0/project/aviadetects/firestore/databases/-default-/data/~2Fdetections"
+}
